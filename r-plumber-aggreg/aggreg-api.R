183d8627ef00cb98eca071bf68117b76b8897457
Winston Sieck <41486655+winstonsieck@users.noreply.github.com>
2024-09-05 14:21:08 -0400

    data aggregation api

---
+++ r-plumber-aggreg/aggreg-api.R
@@ -0,0 +1,19 @@
+# library(jsonlite)
+
+
+#* @apiTitle Aggregate API
+#* @apiDescription API summarizes input employee data
+
+
+#* Returns overall mean age and age by department for input data
+#* @param employee_json
+#* @post /employee-summary
+function(employee_json) {
+  df <- employee_json
+  mean_age <- mean(df$age)
+  dept_mean_ages_df <- aggregate(age~dept, data = df, FUN = mean)
+  dept_mean_ages <- split(dept_mean_ages_df$age, dept_mean_ages_df$dept)
+  list(mean_age = mean_age,
+       dept_mean_ages = dept_mean_ages)
+}
+
